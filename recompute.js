bd283c870c8372291d3f79d8a4739b2b07f009d5
werolist <weroplaylist@proton.me>
2024-09-03 21:29:26 +0530

    auto push at 2024090321291725379166

--- recompute.js
+++ recompute.js
@@ -55,7 +55,7 @@ function refresh() {
     const all = document.getElementsByTagName("all");
 
     if(all) {
-        console.log(all.getAttribute("hidden"));
+        console.log(all);
     }
 }
 
